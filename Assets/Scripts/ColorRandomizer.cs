e6a4f42c5d917603703e02543be1963eb7b3cdc9
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-02 21:51:05 +0100

    Adjusted boobs

---
+++ Assets/Scripts/ColorRandomizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRandomizer : MonoBehaviour {
+    
+    public Color[] colors;
+    public Renderer[] affectedRenderers;
+    
+	void Start ()
+    {
+        Color selectedColor = colors[Random.Range(0, colors.Length)];
+        foreach (Renderer renderer in affectedRenderers)
+        {
+            renderer.material = new Material(renderer.material);    // Make sure to have a private copy of the material
+            renderer.material.color = selectedColor;
+        }
+	}
+}
