aeee76675b595ff147e527cff89023fd481d07b7
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-02 21:27:57 +0100

    Added main scene loading overlay

---
+++ Assets/Scripts/MainSceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneLoader : MonoBehaviour {
+
+    public float delayInSeconds = 1;
+    public Image loadingOverlay;
+
+    private Player player;
+    private float targetTimeToStart;
+    
+	void Start ()
+    {
+        player = FindObjectOfType<Player>();
+        player.gameObject.SetActive(false);
+        targetTimeToStart = Time.realtimeSinceStartup + delayInSeconds;
+        Time.timeScale = 50;
+	}
+	
+	void Update ()
+    {
+		if(Time.realtimeSinceStartup > targetTimeToStart)
+        {
+            Debug.Log("Done");
+            Time.timeScale = 1;
+            player.gameObject.SetActive(true);
+            loadingOverlay.gameObject.SetActive(false);
+        }
+	}
+}
