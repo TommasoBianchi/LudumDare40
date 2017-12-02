5b22d7f52c60a1abc584d1fe2587376e0dc488ee
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-02 15:44:58 +0100

    Created mover and spawner scripts

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed = 0;
+    
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        transform.Translate(transform.forward * speed * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Mover prefab;
+    public GameObject[] spawnPoints;
+    public float meanCooldown;
+    public float cooldownVariance;
+    public float meanSpeed;
+    public float speedVariance;
+
+    private float nextSpawnTime;
+
+    private GaussianDistribution speedGaussianDistribution;
+    private GaussianDistribution cooldownGaussianDistribution;
+
+    void Start ()
+    {
+        speedGaussianDistribution = new GaussianDistribution(meanSpeed, speedVariance);
+        cooldownGaussianDistribution = new GaussianDistribution(meanCooldown, cooldownVariance);
+        nextSpawnTime = Time.time + cooldownGaussianDistribution.Generate();
+    }
+	
+	void Update ()
+    {
+		if(Time.time > nextSpawnTime)
+        {
+            // Spawn something
+            spawn();
+
+            // Update timer
+            nextSpawnTime = Time.time + cooldownGaussianDistribution.Generate();
+        }
+	}
+
+    private void spawn()
+    {
+        GameObject spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        GameObject go = Instantiate(prefab.gameObject, spawnPoint.transform.position, transform.rotation, transform);
+        go.GetComponent<Mover>().speed = speedGaussianDistribution.Generate();
+    }
+}
