30b417e081c17e88d77e112528e8fc8c6872540d
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-03 20:29:18 +0100

    Created Darts minigame

---
+++ Assets/Scripts/Darts.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darts : MonoBehaviour {
+
+    public WaypointWanderer target;
+    public WaypointWanderer aim;
+    public Animator handAnimator;
+    public LayerMask targetLayerMask;
+    
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Shoot!
+            handAnimator.SetTrigger("Throw");
+            target.speed = 0;
+            aim.speed = 0;
+
+            if(Physics.Raycast(aim.transform.position, Vector3.back, 20, targetLayerMask))
+            {
+                FindObjectOfType<MinigameManager>().Win();
+            }
+            else
+            {
+                FindObjectOfType<MinigameManager>().Lose();
+            }
+        }
+	}
+}
