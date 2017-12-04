0583e6038356efad3b787272ca9715ca2b39d968
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-04 14:54:54 +0100

    Real World UI Script

---
+++ Assets/Scripts/RealWorldUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealWorldUI : MonoBehaviour {
+
+    public Vector2 onScreenPosition;
+    public Camera uiCamera;
+    
+	void Start ()
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        Vector3 readOnScreenPosition = new Vector3(onScreenPosition.x * screenWidth, onScreenPosition.y * screenHeight, 1);
+        Vector3 worldPosition = uiCamera.ScreenToWorldPoint(readOnScreenPosition);
+        transform.position = worldPosition;
+    }
+}
