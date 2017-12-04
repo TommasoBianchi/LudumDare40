9456cb0b3509995b5646574cdb2fe51966841bf4
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-12-04 22:42:39 +0100

    Highscore saving

---
+++ Assets/Scripts/HighscoreUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscoreUI : MonoBehaviour
+{
+
+    public Text pointsText;
+    public InputField nameInputField;
+
+    private int points;
+
+    void Start()
+    {
+        points = Mathf.FloorToInt(GameManager.RawDrunkLevel * 1000);
+        pointsText.text = points.ToString();
+    }
+
+    public void SaveHighscore()
+    {
+        GameManager.SaveHighscore(nameInputField.text, points);
+    }
+}
